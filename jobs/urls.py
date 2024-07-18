78852cf5fc0c86a06c4a4f82f7d322452fea5a44
YellowFlash2012 <cyclop9@hotmail.com>
2024-07-18 07:35:09 +0100

    design and implement get_one_job, update_one_job, delete_one_job and get_jobs_bu_topic views, update urls.py and test them in postman.

--- jobs/urls.py
+++ jobs/urls.py
@@ -2,5 +2,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.get_all_jobs, name="get_all_jobs")
+    path("", views.get_all_jobs, name="get_all_jobs"),
+    
+    path("new", views.add_new_job, name="add_new_jobs"),
+    
+    path("<str:pk>", views.get_one_job, name="get_one_job"),
+    
+    path("<str:pk>/update", views.update_one_job, name="update_one_job"),
+    
+    path("<str:pk>/delete", views.delete_one_job, name="delete_one_job"),
+    
+    path("stats/<str:topic>", views.get_topic_stats, name="get_topic_stats"),
 ]
--- jobs/views.py
+++ jobs/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import *
 from .serializers import *
+from django.db.models import Avg, Min, Max, Count
 
 # Create your views here.
 @api_view(["GET"])
@@ -12,4 +13,72 @@ def get_all_jobs(request):
     
     serializer = JobSerializer(jobs, many=True)
     
-    return Response({"success":True, "message":"Here are all the jobs on this platform", "data":serializer.data}, status=status.HTTP_200_OK)
+    return Response({"success":True, "count":len(serializer.data), "message":"Here are all the jobs on this platform", "data":serializer.data}, status=status.HTTP_200_OK)
+
+@api_view(["GET"])
+def get_one_job(request, pk):
+    job = get_object_or_404(Job, id=pk)
+    
+    serializer = JobSerializer(job, many=False)
+    
+    return Response({"success":True, "message":"Here is the job you requested", "data":serializer.data}, status=status.HTTP_200_OK)
+
+@api_view(["POST"])
+def add_new_job(request):
+    data = request.data
+    
+    job = Job.objects.create(**data)
+    
+    serializer = JobSerializer(job, many=False)
+    
+    return Response({"success":True, "message":"The new job was successfully posted!", "data":serializer.data}, status=status.HTTP_201_CREATED)
+
+@api_view(["PUT"])
+def update_one_job(request, pk):
+    job = get_object_or_404(Job, id=pk)
+    
+    job.title = request.data['title']
+    job.description = request.data['description']
+    job.email = request.data['email']
+    job.address = request.data['address']
+    job.jobType = request.data['jobType']
+    job.education = request.data['education']
+    job.industry = request.data['industry']
+    job.experience = request.data['experience']
+    job.salary = request.data['salary']
+    job.positions = request.data['positions']
+    job.company = request.data['company']
+
+    job.save()
+    
+    serializer = JobSerializer(job, many=False)
+    
+    return Response({"success":True, "message":"This job was successfully updated!", "data":serializer.data}, status=status.HTTP_201_CREATED)
+
+@api_view(["DELETE"])
+def delete_one_job(request, pk):
+    job = get_object_or_404(Job, id=pk)
+    
+    job.delete()
+    
+    return Response({"success":True, "message":f"Job {job.id} was successfully deleted!"}, status=status.HTTP_202_ACCEPTED)
+
+@api_view(['GET'])
+def get_topic_stats(request, topic):
+
+    args = { 'title__icontains': topic }
+    jobs = Job.objects.filter(**args)
+
+    if len(jobs) == 0:
+        return Response({ 'message': f'No stats found for {topic}'.format(topic=topic) })
+
+    
+    stats = jobs.aggregate(
+        total_jobs = Count('title'),
+        avg_positions = Avg('positions'),
+        avg_salary = Avg('salary'),
+        min_salary = Min('salary'),
+        max_salary = Max('salary')
+    )
+
+    return Response({"success":True, "message":"Here are all the jobs related to that topic", "data":stats}, status=status.HTTP_200_OK)
